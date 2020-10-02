4f4e28ffb36244c871d0d6a88725e643e8e1ae0f
JennMau99 <35314199+JennMau99@users.noreply.github.com>
2020-10-02 14:25:03 -0500

    Update siteConfig.js
    Started tech portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
+    title: 'Jennifer\'s Tech Portfolio', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://jennmau99.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -67,9 +67,8 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
-        twitter: '',
-        github: '',
+        linkedin: 'jennifer-maunder-93b828157',
+        github: 'JennMau99',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
